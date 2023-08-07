5b945bfe3daae912afc2a0398a1ced2f1f16afad
joe-bor <kimborlongan_08@yahoo.com>
2023-08-07 08:00:01 -0700

    useRef | useEffect| useMemo | useCallback

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,60 @@
-import Counter from "./components/Counter"
-import Heading from "./components/Heading"
-import { Section } from "./components/Section"
-import List from "./components/List"
+import { useState, useEffect, useCallback, useMemo, useRef, MouseEvent, KeyboardEvent } from "react"
 
-import { useState } from "react"
+interface User {
+  id: number,
+  username: string
+}
+
+type FibFunc = (n: number) => number
+
+const fib: FibFunc = (n) => {
+  if (n < 2) return n
+  return fib(n-1) + fib(n-2)
+}
+
+const myNum: number = 37
 
 function App() {
-  const [count, setCount] = useState(1)
+
+  const [count, setCount] = useState<number>(0)
+  const [users, setUsers] = useState<User[] | null>(null)
+
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  console.log(inputRef?.current)
+  console.log(inputRef?.current?.value)
+
+  /**
+   * No TS necessary
+   * - because its supposed to be used for side effects
+   * - always returns void
+   */
+  useEffect( () => {
+    console.log('Mounting')
+    console.log(`Users: ${users}`)
+
+    
+    return () => console.log('Unmounting')
+  }, [users])
+  
+  /**
+   * useCallBack() -> memoizes a function so it doesn't have to be recreated every time
+   */
+  const addTwo = useCallback( (e: MouseEvent<HTMLButtonElement> | KeyboardEvent<HTMLButtonElement>): void => setCount(prev => prev + 2), [])
+
+
+  /**
+   * useMemo()
+   */
+  const result = useMemo( () => fib(myNum), [myNum])
 
   return (
-    <>
-    <Heading title={'Hello World!'}/>
-    <Section title={'Explicit Title'}>This is my Section.</Section>
-    <Counter setCount={setCount}>Count is {count} </Counter>
-    <List items={['A', 'B', 'C', 'D']} render={(item: string) => <span className="gold bold">{item}</span>} />
-    </>
+    <div className="App">
+      <h1>{count}</h1>
+      <button onClick={addTwo}>Add</button>
+      <h2>{result}</h2>
+      <input type="text" name="" id="" ref={inputRef} />
+    </div>
   )
 }
 
